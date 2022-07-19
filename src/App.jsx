2f40110c9f9efafd5283f3109de4ddb681ad4109
PerpetuumG <perpetuumg@gmail.com>
2022-07-19 23:45:07 +0300

    create ui components

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./containers/header";
+import Product from "./containers/product";
+import Posts from "./containers/posts";
+import Order from "./containers/order";
+import Footer from "./containers/footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header></Header>
+            <Product></Product>
+            <Posts></Posts>
+            <Order></Order>
+            <Footer></Footer>
+        </div>
+    );
 }
 
 export default App;
